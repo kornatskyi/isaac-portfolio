601b3eab0b48dbdd43c5935ca0d0f74a08790348
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-07 19:14:46 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+import "./styles/style.scss"
+
+import App from "./App.js";
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
