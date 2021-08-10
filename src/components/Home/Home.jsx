de4301882340336c2ba8a6b0b543ff71bfe1c89b
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-09 18:43:18 -0700

    set ub background and name

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Home.scss";
 import video from "../../assets/videos/background.webm";
-import data from '../../data.json'
+import data from "../../data.json";
 
 export default function Home() {
   return (
     <div className="container">
-      <video autoPlay muted loop className="videoBackground">
-        <source src={video} type="video/mp4" />
-      </video>
-      <h1 className="name">{data.name} </h1>
+    {/* autoPlay */}
+        <video  muted loop className="videoBackground">
+          <source src={video} type="video/mp4" />
+        </video>
+
+        <div className="navigation">
+          <a href="" className="about"></a>
+          
+        </div>
+        <div className="nameContainer">
+          <h1 className="name">{data.name}</h1>
+        </div>
+
     </div>
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import './index.css';
 import "./styles/style.scss"
 
-//Fonts
-import './assets/fonts/Dosis/Dosis-VariableFont_wght.ttf'
 
 import App from "./App.js";
 
