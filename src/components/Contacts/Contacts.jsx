aa869b79bbbe200f9e5c76b62281f64beeb0352f
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-13 20:22:27 -0700

    set up EmailJS

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { addRule } from "../../utils/utilFunctions";
 import data from "../../data.json";
 import emailjs from "emailjs-com";
@@ -13,6 +13,16 @@ import {
 } from "react-icons/ai";
 
 export default function Contacts(props) {
+
+
+
+
+
+
+
+
+  const [forceUpdate, setForceUpdate] = useState("")
+  const [isSending, setIsSending] = useState(false);
   //Change nav links color
   useEffect(() => {
     addRule(".nav-link:after", {
@@ -23,8 +33,15 @@ export default function Contacts(props) {
     });
   }, []);
 
+
+  if(new Date().getTime() > parseInt(window.localStorage.sendingTime)) {
+    window.localStorage.isSent = false;
+  }
+
+
   function sendEmail(e) {
     e.preventDefault();
+    setIsSending(true)
 
     emailjs
       .sendForm(
@@ -35,13 +52,51 @@ export default function Contacts(props) {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          if (result.text === "OK") {
+            console.log(result.text);
+            window.localStorage.isSent = true;
+
+            //1s === 1000ms
+            window.localStorage.sendingTime = new Date().getTime() + 10000;
+            setIsSending(false)
+
+
+          }
         },
         (error) => {
           console.log(error.text);
         }
       );
   }
+
+  const safeForm = () => {
+    if (window.localStorage.isSent === "true") {
+      return (
+        <form className="contact-form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input className="sent" type="submit" disabled value="Message's sent" />
+          <p className="clue">*You'll be able to send an another message after 5min</p>
+        </form>
+      );
+    } else {
+      return (<form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        {isSending? <input className="sending" type="submit" value="Sending" /> : <input type="submit" value="Send" />}
+      </form>)
+    }
+  };
 
   return (
     <div className="contactsContainer">
@@ -78,16 +133,7 @@ export default function Contacts(props) {
         </div>
         <div className="formColumn">
           <p>or just write me a message in the form below</p>
-          <form className="contact-form" onSubmit={sendEmail}>
-            <input type="hidden" name="contact_number" />
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-          </form>
+        {safeForm()}
         </div>
       </div>
     </div>
